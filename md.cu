87807831df5c56d941bb7ecb0e265a25cfeca1bc
Emika Tashiro Hammond <eth@bu.edu>
2024-12-04 09:56:18 -0500

    sync motion update code from liveshare session

--- md.cu
+++ md.cu
@@ -19,7 +19,7 @@
 
 // particle stores coordinates and velocities in x,y,z dimensions
 struct Particle {
-    int particleId;
+    int particle_id;
     float x;
     float y;
     float z;
@@ -75,14 +75,14 @@ __global__ void force_eval(struct Cell *cell_list, float *accelerations)
     home_cell.particle_list[threadIdx.x] = cell_list[home_x][home_y][home_z].particle_list[threadIdx.x];
     neighbor_cell.particle_list[threadIdx.x] = cell_list[neighbor_x][neighbor_y][neighbor_z].particle_list[threadIdx.x];
 
-    int reference_particle_id = home_cell.particle_list[threadIdx.x].particleId;
+    int reference_particle_id = home_cell.particle_list[threadIdx.x].particle_id;
     if (reference_particle_id == -1)
         return;
 
     __syncthreads();
 
     //TODO: (easy) write the rest of the force computation
-    for (int i = 0; neighbor_cell.particle_list[i].particleId != -1 && i < MAX_PARTICLES_PER_CELL; ++i) {
+    for (int i = 0; neighbor_cell.particle_list[i].particle_id != -1 && i < MAX_PARTICLES_PER_CELL; ++i) {
         // boolean expression can be optimized knowing the fact that one dimension of the neighboring half shell is only +1 and not -1
         float neighbor_particle_virtual_x = neighbor_cell.particle_list[i].x + ((home_x - neighbor_x == CELL_LENGTH_X - 1) + (neighbor_x - home_x == CELL_LENGTH_X - 1) * -1) * (CELL_LENGTH_X * CELL_CUTOFF_RADIUS);
         float neighbor_particle_virtual_y = neighbor_cell.particle_list[i].y + ((home_y - neighbor_y == CELL_LENGTH_Y - 1) + (neighbor_y - home_y == CELL_LENGTH_Y - 1) * -1) * (CELL_LENGTH_Y * CELL_CUTOFF_RADIUS);
@@ -94,42 +94,49 @@ __global__ void force_eval(struct Cell *cell_list, float *accelerations)
         atomicAdd(&accelerations[reference_particle_id][2], compute_force(home_cell.particle_list[threadIdx.x].z, neighbor_particle_virtual_z));
     }
 
-    // particle update here, all particles are still in their original cell
+    // choose one block to "work" on the home cell
+    // threads update their associated particle here
+    // all particles are still in their original cell
+    if (local_idx != 0)
+        return;
+    home_cell.particle_list[threadIdx.x].vx += accelerations[reference_particle_id] * TIMESTEP_DURATION;
+    home_cell.particle_list[threadIdx.x].vy += accelerations[reference_particle_id + 1] * TIMESTEP_DURATION;
+    home_cell.particle_list[threadIdx.x].vz += accelerations[reference_particle_id + 2] * TIMESTEP_DURATION;
+    home_cell.particle_list[threadIdx.x].x = (home_cell.particle_list[threadIdx.x].x + home_cell.particle_list[threadIdx.x].vx * TIMESTEP_DURATION) - (CELL_LENGTH_X * CELL_CUTOFF_RADIUS) * floor(home_cell.particle_list[threadIdx.x].x / (CELL_LENGTH_X * CELL_CUTOFF_RADIUS));
+    home_cell.particle_list[threadIdx.x].y = (home_cell.particle_list[threadIdx.x].y + home_cell.particle_list[threadIdx.x].vy * TIMESTEP_DURATION) - (CELL_LENGTH_Y * CELL_CUTOFF_RADIUS) * floor(home_cell.particle_list[threadIdx.x].y / (CELL_LENGTH_Y * CELL_CUTOFF_RADIUS));
+    home_cell.particle_list[threadIdx.x].z = (home_cell.particle_list[threadIdx.x].z + home_cell.particle_list[threadIdx.x].vz * TIMESTEP_DURATION) - (CELL_LENGTH_Z * CELL_CUTOFF_RADIUS) * floor(home_cell.particle_list[threadIdx.x].z / (CELL_LENGTH_Z * CELL_CUTOFF_RADIUS));
+
+    // update global
+    cell_list[home_z + home_y * CELL_LENGTH_Z + home_x * CELL_LENGTH_Z * CELL_LENGTH_Y].particle_list[threadIdx.x] = cell.particle_list[threadIdx.x];
 }
 
+// update cell lists because particles have moved
 __global__ void motion_update(struct Cell *cell_list, float *accelerations)
 {
-    // think about particles moving from cell to cell
-    int cell_x = blockIdx.x % CELL_LENGTH_X;
-    int cell_y = blockIdx.x / CELL_LENGTH_X % CELL_LENGTH_Y;
-    int cell_z = blockIdx.x / (CELL_LENGTH_X * CELL_LENGTH_Y) % CELL_LENGTH_Z;
+    // get home cell coordinates
+    int home_x = blockIdx.x % CELL_LENGTH_X;
+    int home_y = blockIdx.x / CELL_LENGTH_X % CELL_LENGTH_Y;
+    int home_z = blockIdx.x / (CELL_LENGTH_X * CELL_LENGTH_Y) % CELL_LENGTH_Z;
 
     __shared__ struct Cell cell;
     cell.particle_list[threadIdx.x] = cell_list[cell_x + cell_y * CELL_LENGTH_X + cell_z * CELL_LENGTH_X * CELL_LENGTH_Y].particle_list[threadIdx.x];
 
-    int particleId = cell.particle_list[threadIdx.x].particleId;
+    int particle_id = cell.particle_list[threadIdx.x].particle_id;
 
-    // python: cell_from_position = lambda r: linear_idx(*[floor(x/CUTOFF)%UNIVERSE_SIZE for x in r])
-    cell.particle_list[threadIdx.x].vx += accelerations[particleId] * TIMESTEP_DURATION;
-    cell.particle_list[threadIdx.x].vy += accelerations[particleId + 1] * TIMESTEP_DURATION;
-    cell.particle_list[threadIdx.x].vz += accelerations[particleId + 2] * TIMESTEP_DURATION;
-    cell.particle_list[threadIdx.x].x = (cell.particle_list[threadIdx.x].x + cell.particle_list[threadIdx.x].vx * TIMESTEP_DURATION) - (CELL_LENGTH_X * CELL_CUTOFF_RADIUS) * floor(cell.particle_list[threadIdx.x].x / (CELL_LENGTH_X * CELL_CUTOFF_RADIUS));
-    cell.particle_list[threadIdx.x].y = (cell.particle_list[threadIdx.x].y + cell.particle_list[threadIdx.x].vy * TIMESTEP_DURATION) - (CELL_LENGTH_Y * CELL_CUTOFF_RADIUS) * floor(cell.particle_list[threadIdx.x].y / (CELL_LENGTH_Y * CELL_CUTOFF_RADIUS));
-    cell.particle_list[threadIdx.x].z = (cell.particle_list[threadIdx.x].z + cell.particle_list[threadIdx.x].vz * TIMESTEP_DURATION) - (CELL_LENGTH_Z * CELL_CUTOFF_RADIUS) * floor(cell.particle_list[threadIdx.x].z / (CELL_LENGTH_Z * CELL_CUTOFF_RADIUS));
-
-    cell_list[cell_x + cell_y * CELL_LENGTH_X + cell_z * CELL_LENGTH_X * CELL_LENGTH_Y].particle_list[threadIdx.x] = cell.particle_list[threadIdx.x];
-
-    // put above code in force_eval. this code is supposed to 
     // update cell list with updated particles
     // one block per cell
     // one thread per cell not equal to block's cell
     // that thread loops over each particle and copies it to home cell's particle list
+    int new_cell_x = cell_particle_list[threadIdx.x].x / (CELL_LENGTH_X * CELL_CUTOFF_RADIUS);
+    int new_cell_y = cell_particle_list[threadIdx.x].y / (CELL_LENGTH_Y * CELL_CUTOFF_RADIUS);
+    int new_cell_z = cell_particle_list[threadIdx.x].z / (CELL_LENGTH_Z * CELL_CUTOFF_RADIUS);
 
-    accelerations[particleId] = 0;
-    accelerations[particleId + 1] = 0;
-    accelerations[particleId + 2] = 0;
+    accelerations[particle_id] = 0;
+    accelerations[particle_id + 1] = 0;
+    accelerations[particle_id + 2] = 0;
 }
 
+// initialize cells with random particle data
 void initialize_cell_list(struct Cell cellList[CELL_LENGTH_X * CELL_LENGTH_Y * CELL_LENGTH_Z])
 {
         // initialize cell list, -1 for empty cell
@@ -139,8 +146,9 @@ void initialize_cell_list(struct Cell cellList[CELL_LENGTH_X * CELL_LENGTH_Y * C
                 int y = rand() % CELL_LENGTH_Y;
                 int z = rand() % CELL_LENGTH_Z;
 
+                // assign random particle data
                 struct Particle particle = {
-                        .particleId = i,
+                        .particle_id = i,
                         .x = x * CELL_CUTOFF_RADIUS + ((float) rand() / RAND_MAX) * CELL_CUTOFF_RADIUS,
                         .y = y * CELL_CUTOFF_RADIUS + ((float) rand() / RAND_MAX) * CELL_CUTOFF_RADIUS,
                         .z = z * CELL_CUTOFF_RADIUS + ((float) rand() / RAND_MAX) * CELL_CUTOFF_RADIUS,
@@ -148,9 +156,9 @@ void initialize_cell_list(struct Cell cellList[CELL_LENGTH_X * CELL_LENGTH_Y * C
                         .vy = 0,
                         .vz = 0,
                 };
-                // copy particle to 
+                // copy particle to to cell list
                 for (int j = 0; j < MAX_PARTICLES_PER_CELL; ++j) {
-                    if (cellList[x][y][z].particle_list[j].particleId == -1) {
+                    if (cellList[x][y][z].particle_list[j].particle_id == -1) {
                         memcpy(&cellList[x][y][z].particle_list[j], &particle, sizeof(struct Particle));
                         break;
                     }
@@ -178,10 +186,10 @@ int main()
     //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     /*
         accelerations stores accelerations (in x y z dimensions) of each particle to be used in motion update.
-        - index of accelerations is related to particleId
-        - particleId * 3 gives index of accelerations for x dimension
-        - (particleId * 3) + 1 gives index of y
-        - (particleId * 3) + 2 gives index of y
+        - index of accelerations is related to particle_id
+        - particle_id * 3 gives index of accelerations for x dimension
+        - (particle_id * 3) + 1 gives index of y
+        - (particle_id * 3) + 2 gives index of y
     */
     float *accelerations;
     cudaMalloc(&accelerations, MAX_PARTICLES_PER_CELL * 3 * sizeof(float));
@@ -193,9 +201,11 @@ int main()
     //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     // defines block and thread dimensions
     // dim3 is an integer vector type most commonly used to pass the grid and block dimensions in a kernel invocation [X x Y x Z]
-    dim3 numBlocksForce(CELL_LENGTH_X * CELL_LENGTH_Y * CELL_LENGTH_Z * 14);    // (CELL_LENGTH_X * CELL_LENGTH_Y * CELL_LENGTH_Z * 14) x 1 x 1
-    dim3 numBlocksMotion(CELL_LENGTH_X * CELL_LENGTH_Y * CELL_LENGTH_Z);        // (CELL_LENGTH_X * CELL_LENGTH_Y * CELL_LENGTH_Z) x 1 x 1
-    dim3 threadsPerBlock(MAX_PARTICLES_PER_CELL);                               // MAX_PARTICLES_PER_CELL x 1 x 1
+    // there are 2^31 blocks in x dimension while y and z have at most 65536 blocks
+    dim3 numBlocksForce(CELL_LENGTH_X * CELL_LENGTH_Y * CELL_LENGTH_Z * 14);        // (CELL_LENGTH_X * CELL_LENGTH_Y * CELL_LENGTH_Z * 14) x 1 x 1
+    dim3 numBlocksMotion(CELL_LENGTH_X * CELL_LENGTH_Y * CELL_LENGTH_Z);            // (CELL_LENGTH_X * CELL_LENGTH_Y * CELL_LENGTH_Z) x 1 x 1
+    dim3 threadsPerBlockForce(MAX_PARTICLES_PER_CELL);                              // MAX_PARTICLES_PER_CELL x 1 x 1
+    dim3 threadsPerBlockMotion(CELL_LENGTH_X, CELL_LENGTH_Y, CELL_LENGTH_Z);  
 
     //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     // FORCE COMPUTATION AND MOTION UPDATE
@@ -204,8 +214,8 @@ int main()
     // steps are separated to ensure threads are synchronized (that force_eval is done)
     // output of force_eval is stores in device_cell_list and accelerations
     for (int t = 0; t < TIMESTEPS; ++t) {
-        force_eval<<<numBlocksForce, threadsPerBlock>>>(device_cell_list, accelerations);
-        motion_update<<<numBlocksMotion, threadsPerBlock>>>(device_cell_list, accelerations);
+        force_eval<<<numBlocksForce, threadsPerBlockForce>>>(device_cell_list, accelerations);
+        motion_update<<<numBlocksMotion, threadsPerBlockMotion>>>(device_cell_list, accelerations);
     }
 
     //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
