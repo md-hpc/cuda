82f87c750d6b5eb9ff1039e424769250bf86f4d4
ajamias <ajamias@bu.edu>
2025-02-17 20:45:55 -0500

    Update PDB importer

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-CC = nvcc
+CC = gcc
 CFLAGS = -I./include
 
 TESTS_DIR = tests
@@ -7,17 +7,21 @@ BUILD_DIR = build
 
 .PHONY: tests run_tests clean
 
-TESTS = test_pdb_importer #test_cell_list
+TESTS = test_pdb_importer test_naive #test_cell_list
 
 tests: $(TESTS)
 
 run_tests: tests
 	$(BUILD_DIR)/test_pdb_importer $(TESTS_DIR)/input.pdb $(TESTS_DIR)/expected.pdb
+	$(BUILD_DIR)/test_naive $(TESTS_DIR)/input.pdb
 
 $(BUILD_DIR):
 	mkdir -p $(BUILD_DIR)
 
 test_pdb_importer: $(TESTS_DIR)/test_pdb_importer.c $(SRC_DIR)/pdb_importer.c | $(BUILD_DIR)
+	$(CC) $(CFLAGS) $^ -o $(BUILD_DIR)/$@
+
+test_naive: $(TESTS_DIR)/test_naive.c $(SRC_DIR)/naive.c | $(BUILD_DIR)
 	$(CC) $(CFLAGS) $^ -o $(BUILD_DIR)/$@
 
 #test_cell_list: $(SRC_DIR)/md.cu $(SRC_DIR)/pdb_importer.c | $(BUILD_DIR)
--- include/pdb_importer.h
+++ include/pdb_importer.h
@@ -1,6 +1,28 @@
 #ifndef PDB_IMPORTER_H
 #define PDB_IMPORTER_H
 
+#define MAX_PARTICLES_PER_CELL 128
+
+#ifndef CELL_LENGTH_X
+#define CELL_LENGTH_X 3
+#endif
+
+#ifndef CELL_LENGTH_Y
+#define CELL_LENGTH_Y 3
+#endif
+
+#ifndef CELL_LENGTH_Z
+#define CELL_LENGTH_Z 3
+#endif
+
+#ifndef TIMESTEPS
+#define TIMESTEPS 1
+#endif
+
+#ifndef TIMESTEP_DURATION_FS
+#define TIMESTEP_DURATION_FS 1                            
+#endif
+
 struct Particle {
     int particle_id;
     float x;
@@ -30,14 +52,11 @@ int import_atoms(const char *const filename, struct Particle **particle_list, in
  * @brief Assigns particles to a spatial cell grid based on their positions.
  *
  * This function distributes particles into a grid of cells for efficient spatial
- * partitioning, which can be useful for simulations such as molecular dynamics
- * or particle-based physics computations.
+ * partitioning.
  *
  * @param particle_list      Pointer to an array of particles.
  * @param particle_count     Number of particles in the particle_list.
  * @param cell_list          Pointer to an array of cells that store particles.
- * @param x_dim              Number of cells along the x-axis.
- * @param y_dim              Number of cells along the y-axis.
  * @param cell_cutoff_radius The size of each cell, defining the partitioning scale.
  *
  * @note This function assumes that the `particle_list` contains valid particle data
@@ -45,6 +64,6 @@ int import_atoms(const char *const filename, struct Particle **particle_list, in
  *       have sufficient memory allocated to store the particles.
  */
 void create_cell_list(struct Particle *particle_list, int particle_count,
-                      struct Cell *cell_list, int x_dim, int y_dim, int cell_cutoff_radius);
+                      struct Cell *cell_list, int cell_cutoff_radius);
 
 #endif /* PDB_IMPORTER_H */
