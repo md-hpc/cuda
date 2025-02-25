02d5cce703404b18699fb310037f776aebda4162
Emika Hammond <eth@scc1.bu.edu>
2025-02-25 17:14:13 -0500

    modify scripts

--- scripts/nsquared_ts-1_tsd-1e-15.sh
+++ scripts/nsquared_ts-1_tsd-1e-15.sh
@@ -9,5 +9,5 @@
 module load cuda/11.3
 
 nvcc -I./include -D TIMESTEPS=1 -D TIMESTEP_DURATION_FS=1e-15 src/pdb_importer.c src/nsquared.cu -o build/nsquared 
-time build/nsquared tests/example_input.pdb tests/output.csv
+time build/nsquared tests/example_input.pdb tests/ts-1_tsd-1e-15_output.csv > ts-1_tsd-1e-15-output.txt
 
