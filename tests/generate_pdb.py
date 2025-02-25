53352bf77176f8020063288bdac6c219a239c097
Emika Tashiro Hammond <eth@bu.edu>
2025-02-25 10:35:30 -0500

    add cell lengths in each dimension as input

--- tests/generate_pdb.py
+++ tests/generate_pdb.py
@@ -4,23 +4,20 @@ from collections import defaultdict
 
 MAX_PARTICLES_PER_CELL = 128
 
-def generate_pdb(filename, num_particles, box_size, cell_cutoff_radius):
+def generate_pdb(filename, num_particles, cell_length_x, cell_length_y, cell_length_z, cell_cutoff_radius):
     # need to check for unique positions
     positions = set() 
     cell_counts = defaultdict(int)
 
-    cell_size = cell_cutoff_radius
-    num_cells_per_dim = int(box_size/cell_size)
-
     with open(filename, "w") as f:
         while len(positions) < num_particles:
-            x = round(random.uniform(0, box_size), 3)
-            y = round(random.uniform(0, box_size), 3)
-            z = round(random.uniform(0, box_size), 3)
-            cell_x = int(x/cell_size)
-            cell_y = int(y/cell_size)
-            cell_z = int(z/cell_size)
-            cell_index = (cell_x, cell_y, cell_z)
+            x = round(random.uniform(0, cell_length_x), cell_cutoff_radius)
+            y = round(random.uniform(0, cell_length_y), cell_cutoff_radius)
+            z = round(random.uniform(0, cell_length_z), cell_cutoff_radius)
+            x_cell = int(x/cell_cutoff_radius)
+            y_cell = int(y/cell_cutoff_radius)
+            z_cell = int(z/cell_cutoff_radius)
+            cell_index = (x_cell, y_cell, z_cell)
 
             if cell_counts[cell_index] < MAX_PARTICLES_PER_CELL:
                 position = (x,y,z)
@@ -36,7 +33,9 @@ def main():
     parser = argparse.ArgumentParser(description="generate PDB file with unique particles in random positions")
     parser.add_argument("-o", "--output", type=str, required=True, help="output PDB filename")
     parser.add_argument("-n", "--num_particles", type=int, required=True, help="number of particles")
-    parser.add_argument("-b", "--box_size", type=int, required=True, help="size of cube defined by one dimension in Angstroms")
+    parser.add_argument("-x", "--cell_length_x", type=int, required=True, help="cell length in the x dimension in Angstroms")
+    parser.add_argument("-y", "--cell_length_y", type=int, required=True, help="cell length in the y dimension in Angstroms")
+    parser.add_argument("-z", "--cell_length_z", type=int, required=True, help="cell length in the z dimension in Angstroms")
     parser.add_argument("-c", "--cell_cutoff_radius", type=int, required=True, help="size of cube defined by one dimension in Angstroms")
     
 
