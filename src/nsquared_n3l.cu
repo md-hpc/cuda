dbebe2c1815ed17b06a5f7129ba9752f1370655b
ajamias <ajamias@bu.edu>
2025-04-01 00:25:53 -0400

    fix bug

--- src/nsquared_n3l.cu
+++ src/nsquared_n3l.cu
@@ -223,7 +223,7 @@ int main(int argc, char **argv)
         GPU_PERROR(cudaMemset(accelerations, 0, ((particle_count - 1) / MAX_PARTICLES_PER_BLOCK + 1) * particle_count * sizeof(float) * 3));
         if (t % 2 == 1) {
             compute_acceleration<<<numBlocks, threadsPerBlock>>>(device_particle_ids, device_x_1, device_y_1, device_z_1, vx, vy, vz, device_x_2, device_y_2, device_z_2, accelerations, particle_count);
-            position_update<<<numBlocks, threadsPerBlock>>>(device_x_1, device_y_1, device_z_1, device_x_2, device_y_2, device_z_2, particle_count, accelerations);
+            position_update<<<numBlocks, threadsPerBlock>>>(device_x_1, device_y_1, device_z_1, vx, vy, vz, device_x_2, device_y_2, device_z_2, particle_count, accelerations);
 #ifdef SIMULATE
             GPU_PERROR(cudaMemcpy(host_x, device_x_2, particle_count * sizeof(float), cudaMemcpyDeviceToHost));
             GPU_PERROR(cudaMemcpy(host_y, device_y_2, particle_count * sizeof(float), cudaMemcpyDeviceToHost));
@@ -231,7 +231,7 @@ int main(int argc, char **argv)
 #endif
         } else {
             compute_acceleration<<<numBlocks, threadsPerBlock>>>(device_particle_ids, device_x_2, device_y_2, device_z_2, vx, vy, vz, device_x_1, device_y_1, device_z_1, accelerations, particle_count);
-            position_update<<<numBlocks, threadsPerBlock>>>(device_x_2, device_y_2, device_z_2, device_x_1, device_y_1, device_z_1, particle_count, accelerations);
+            position_update<<<numBlocks, threadsPerBlock>>>(device_x_2, device_y_2, device_z_2, vx, vy, vz, device_x_1, device_y_1, device_z_1, particle_count, accelerations);
 #ifdef SIMULATE
             GPU_PERROR(cudaMemcpy(host_x, device_x_1, particle_count * sizeof(float), cudaMemcpyDeviceToHost));
             GPU_PERROR(cudaMemcpy(host_y, device_y_1, particle_count * sizeof(float), cudaMemcpyDeviceToHost));
