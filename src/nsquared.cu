e299b65c24773c70cbedf3b9eb3179517081339d
Emika Hammond <eth@bu.edu>
2025-04-02 23:31:54 -0400

    add validation to nsquared

--- src/nsquared.cu
+++ src/nsquared.cu
@@ -23,6 +23,16 @@ extern "C" {
 #define R_MIN (0.0317782790163)
 #define LJMAX_ACCELERATION (24.7987876375)
 //constexpr float LJMAX = (4.0f * 24.0f * EPSILON / SIGMA * (0.216344308307f - 2.0f * 0.0582465445441f));
+#ifdef VALIDATE
+__device__ float compute_potential(float r_angstrom) {
+        // validation code for getting potential energy
+        float temp = SIGMA / r_angstrom;
+        temp = temp * temp; // ^2
+        temp = temp * temp * temp; // ^ 6
+        float potential = 4 * EPSILON * ((temp * temp) - temp);
+        return potential;
+}
+#endif
 
 __device__ float compute_acceleration(float r_angstrom) {
         if (r_angstrom < R_MIN)
@@ -30,15 +40,17 @@ __device__ float compute_acceleration(float r_angstrom) {
 
         // in A / s^2
         float temp = SIGMA / r_angstrom;
-        temp = temp * temp;
-        temp = temp * temp * temp;
+        temp = temp * temp; // ^2
+        temp = temp * temp * temp; // ^ 6
+
         return 24 * EPSILON * (2 * temp * temp - temp) / (r_angstrom * ARGON_MASS);
         //float force = 4 * EPSILON * (12 * pow(SIGMA, 12.0f) / pow(r, 13.0f) - 6 * pow(SIGMA, 6.0f) / pow(r, 7.0f)) / ARGON_MASS;
+
 }
 
 __global__ void timestep(float *particle_id, float *src_x, float *src_y, float *src_z,
                          float *vx, float *vy, float *vz, float *dst_x, float *dst_y,
-                         float *dst_z, int particle_count)
+                         float *dst_z, int particle_count, float *device_pe)
 {
     // each thread gets a particle as a reference particle
     int reference_particle_idx = blockIdx.x * blockDim.x + threadIdx.x;
@@ -56,6 +68,7 @@ __global__ void timestep(float *particle_id, float *src_x, float *src_y, float *
     float ax = 0;
     float ay = 0;
     float az = 0;
+
     for (int i = 1; i < particle_count; ++i) {
         // use temp variables to optimize
         float diff_x = reference_x - src_x[(reference_particle_idx + i) % particle_count];
@@ -75,6 +88,9 @@ __global__ void timestep(float *particle_id, float *src_x, float *src_y, float *
         ax += acceleration * diff_x;
         ay += acceleration * diff_y;
         az += acceleration * diff_z;
+#ifdef VALIDATE
+        device_pe[i] = compute_potential(norm);
+#endif
     }
 
     // obtain current velocity of reference particle
@@ -135,8 +151,13 @@ int main(int argc, char **argv)
     float *vx;
     float *vy;
     float *vz;
+    float *device_pe;
 
     import_atoms(input_file, &host_particle_ids, &host_x, &host_y, &host_z, &particle_count);
+    float *host_vx = (float *)malloc(particle_count * sizeof(float));
+    float *host_vy = (float *)malloc(particle_count * sizeof(float));
+    float *host_vz = (float *)malloc(particle_count * sizeof(float));
+    float *host_pe = (float *)malloc(particle_count * sizeof(float));
 
     GPU_PERROR(cudaMalloc(&device_particle_ids, particle_count * sizeof(int)));
     GPU_PERROR(cudaMalloc(&device_x_1, particle_count * sizeof(float)));
@@ -145,9 +166,11 @@ int main(int argc, char **argv)
     GPU_PERROR(cudaMalloc(&device_x_2, particle_count * sizeof(float)));
     GPU_PERROR(cudaMalloc(&device_y_2, particle_count * sizeof(float)));
     GPU_PERROR(cudaMalloc(&device_z_2, particle_count * sizeof(float)));
+    // need for validation
     GPU_PERROR(cudaMalloc(&vx, particle_count * sizeof(float)));
     GPU_PERROR(cudaMalloc(&vy, particle_count * sizeof(float)));
     GPU_PERROR(cudaMalloc(&vz, particle_count * sizeof(float)));
+    GPU_PERROR(cudaMalloc(&device_pe, particle_count * sizeof(float)));
 
     GPU_PERROR(cudaMemcpy(device_particle_ids, host_particle_ids, particle_count * sizeof(int), cudaMemcpyHostToDevice));
     GPU_PERROR(cudaMemcpy(device_x_1, host_x, particle_count * sizeof(float), cudaMemcpyHostToDevice));
@@ -172,20 +195,41 @@ int main(int argc, char **argv)
     clock_gettime(CLOCK_REALTIME, &time_start);
 #endif
 
+#ifdef VALIDATE
+    float momentum_x = 0;
+    float momentum_y = 0;
+    float momentum_z = 0; 
+
+    float potential_energy = 0;
+    float kinetic_energy = 0;
+#endif
+
     for (int t = 1l; t <= TIMESTEPS; ++t) {
         if (t % 2 == 1) {
-            timestep<<<numBlocks, threadsPerBlock>>>(device_particle_ids, device_x_1, device_y_1, device_z_1, vx, vy, vz, device_x_2, device_y_2, device_z_2, particle_count);
+            timestep<<<numBlocks, threadsPerBlock>>>(device_particle_ids, device_x_1, device_y_1, device_z_1, vx, vy, vz, device_x_2, device_y_2, device_z_2, particle_count, device_pe);
 #ifdef SIMULATE
             GPU_PERROR(cudaMemcpy(host_x, device_x_2, particle_count * sizeof(float), cudaMemcpyDeviceToHost));
             GPU_PERROR(cudaMemcpy(host_y, device_y_2, particle_count * sizeof(float), cudaMemcpyDeviceToHost));
             GPU_PERROR(cudaMemcpy(host_z, device_z_2, particle_count * sizeof(float), cudaMemcpyDeviceToHost));
 #endif
+#ifdef VALIDATE
+            GPU_PERROR(cudaMemcpy(host_pe, device_pe, particle_count * sizeof(float), cudaMemcpyDeviceToHost));
+            GPU_PERROR(cudaMemcpy(host_vx, vx, particle_count * sizeof(float), cudaMemcpyDeviceToHost));
+            GPU_PERROR(cudaMemcpy(host_vy, vy, particle_count * sizeof(float), cudaMemcpyDeviceToHost));
+            GPU_PERROR(cudaMemcpy(host_vz, vz, particle_count * sizeof(float), cudaMemcpyDeviceToHost));
+#endif
         } else {
-            timestep<<<numBlocks, threadsPerBlock>>>(device_particle_ids, device_x_2, device_y_2, device_z_2, vx, vy, vz, device_x_1, device_y_1, device_z_1, particle_count);
+            timestep<<<numBlocks, threadsPerBlock>>>(device_particle_ids, device_x_2, device_y_2, device_z_2, vx, vy, vz, device_x_1, device_y_1, device_z_1, particle_count, device_pe);
 #ifdef SIMULATE
             GPU_PERROR(cudaMemcpy(host_x, device_x_1, particle_count * sizeof(float), cudaMemcpyDeviceToHost));
             GPU_PERROR(cudaMemcpy(host_y, device_y_1, particle_count * sizeof(float), cudaMemcpyDeviceToHost));
             GPU_PERROR(cudaMemcpy(host_z, device_z_1, particle_count * sizeof(float), cudaMemcpyDeviceToHost));
+#endif
+#ifdef VALIDATE
+            GPU_PERROR(cudaMemcpy(host_pe, device_pe, particle_count * sizeof(float), cudaMemcpyDeviceToHost));
+            GPU_PERROR(cudaMemcpy(host_vx, vx, particle_count * sizeof(float), cudaMemcpyDeviceToHost));
+            GPU_PERROR(cudaMemcpy(host_vy, vy, particle_count * sizeof(float), cudaMemcpyDeviceToHost));
+            GPU_PERROR(cudaMemcpy(host_vz, vz, particle_count * sizeof(float), cudaMemcpyDeviceToHost));
 #endif
         }
 #ifdef SIMULATE
@@ -193,6 +237,24 @@ int main(int argc, char **argv)
             fprintf(out, "%d,%f,%f,%f\n", host_particle_ids[i], host_x[i], host_y[i], host_z[i]);
         }
         fprintf(out, "\n");
+#endif
+#ifdef VALIDATE
+        for (int i = 0; i < particle_count; ++i) {
+            // accumulate momentum
+            momentum_x += host_vx[i];
+            momentum_y += host_vy[i];
+            momentum_z += host_vz[i];
+            // calculate kinetic energy through velocity
+            float particle_kinetic_energy = 0.5 * ARGON_MASS * ((host_vx[i] * host_vx[i]) + (host_vy[i] * host_vy[i]) + (host_vz[i] * host_vz[i]));
+            // printf("velocity x %.6f\n", host_vx[i]);
+            potential_energy += host_pe[i];
+            // printf("potential x %.6f\n", host_pe[i]);
+            kinetic_energy += particle_kinetic_energy;
+        }
+        printf("potential %.12f + kinetic %.12f = total energy %.12f\n", potential_energy, kinetic_energy, potential_energy + kinetic_energy);
+        printf("momentum x: %.12f\n", momentum_x);
+        printf("momentum y: %.12f\n", momentum_y);
+        printf("momentum z: %.12f\n", momentum_z);
 #endif
     }
 
@@ -213,12 +275,14 @@ int main(int argc, char **argv)
         GPU_PERROR(cudaMemcpy(host_x, device_x_2, particle_count * sizeof(float), cudaMemcpyDeviceToHost));
         GPU_PERROR(cudaMemcpy(host_y, device_y_2, particle_count * sizeof(float), cudaMemcpyDeviceToHost));
         GPU_PERROR(cudaMemcpy(host_z, device_z_2, particle_count * sizeof(float), cudaMemcpyDeviceToHost));
+
+
     } else {
         GPU_PERROR(cudaMemcpy(host_x, device_x_1, particle_count * sizeof(float), cudaMemcpyDeviceToHost));
         GPU_PERROR(cudaMemcpy(host_y, device_y_1, particle_count * sizeof(float), cudaMemcpyDeviceToHost));
         GPU_PERROR(cudaMemcpy(host_z, device_z_1, particle_count * sizeof(float), cudaMemcpyDeviceToHost));
     }
-        
+
     FILE *out = fopen(output_file, "w");
     fprintf(out, "particle_id,x,y,z\n");
     for (int i = 0; i < particle_count; ++i) {
