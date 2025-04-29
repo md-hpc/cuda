d4fa6c61b977fc743b60f593176642d4eb7d620a
Emika Hammond <eth@bu.edu>
2025-04-29 11:08:17 -0400

    fix qsub part fo script generator

--- scripts/script_generator.sh
+++ scripts/script_generator.sh
@@ -99,7 +99,7 @@ for implementations in nsquared nsquared_shared nsquared_n3l cell_list cell_list
 	rm -rf ${PROJ_DIR}/output/\${implementations}/*
 
 	for particle_count in 1024 4096 16384 65536; do
-		qsub ${PROJ_DIR}/scripts/\${implementations}_ts_${TIMESTEPS}_tsd_${TIMESTEP_DURATION}_n_${particle_count}.sh
+		qsub ${PROJ_DIR}/scripts/\${implementations}_ts_${TIMESTEPS}_tsd_${TIMESTEP_DURATION}_n_\${particle_count}.sh
 	done
 done
 EOF
